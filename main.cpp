80372a2dd48977a0847a8585e8d1dfc67f703849
ophelan <phelan.eoin@gmail.com>
2016-03-15 23:32:38 -0400

    thing driver update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,18 @@
 #include <regex>
 #include "object.h"
 
+using namespace std;
+
 int main(void){
 	object thing;
-	cout << "X-Position = " << thing.getx() << endl;
-	cout << "Y-Position = " <<thing.gety() << endl;
+
+	while(1){
+		cout << string( 100, '\n' );
+		cout << "X-Position = " << thing.get_x() << endl;
+		cout << "Y-Position = " << thing.get_y() << endl;
+		cout << "Delta X    = " << thing.get_dx() << endl;
+		cout << "Delta Y    = " << thing.get_dy() << endl;
+		sleep(1);
+		thing.makeChange();
+	}
 }
--- object.h
+++ object.h
@@ -14,7 +14,9 @@ class object{
 	public:
 		object();			
 		int get_x(void);						//ALL FUNCTIONS WHICH RETURN OBJECT VALUES START W/ 'get'
-		int get_y(void);								
+		int get_y(void);
+		int get_dx(void);						//ALL FUNCTIONS WHICH RETURN OBJECT VALUES START W/ 'get'
+		int get_dy(void);								
 		void change_x(int);					//ALL FUNCTIONS WHICH CHANGE OBJECT VALUES START W/ 'change'
 		void change_y(int);
 		void change_dx(int);
@@ -36,10 +38,11 @@ class object{
   	std::cin >> xPos;
   	cout << "Please Enter a Value for the yPos: " << endl;
   	cin >> yPos;
+  	dxVal=1;
+  	dyVal=0;
 	}
-
-//################ BASIC UTILITIES ############################
-
+//############# BASIC RETURN FUNCTIONS ########################
+	
 	int object::get_x(){
 		return xPos;
 	}
@@ -47,6 +50,16 @@ class object{
 	int object::get_y(){
 		return yPos;
 	}
+
+	int object::get_dx(){
+		return dxVal;
+	}
+
+	int object::get_dy(){
+		return dyVal;
+	}
+
+//################ BASIC UTILITIES ############################
 
 	void object::change_x(int xChange){
 		xPos=xPos+xChange;
